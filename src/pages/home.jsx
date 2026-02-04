dc7a34aa7e4710125e47898f50041d71c179e15d
mironshoxshukurov1-art <mironshoxshukurov1@gmail.com>
2026-02-04 19:04:18 +0500

    fixed

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home1 from '../components/Home/home1'
+import Home1 from '../components/Home/Home1'
 import Home2 from '../components/Home/Home2'
 import Home3 from '../components/Home/Home3'
 import Home4 from '../components/Home/Home4'
